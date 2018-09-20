438ca34a781444f6bd431b967562fbed67408c61
Pixelhash <dev@codehat.de>
2018-09-20 16:07:00 +0200

    Update docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'SignColors',
+  description: 'Allow players on your server to write with colors and formats on signs.',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Commands & Permissions', link: '/cmdandperms/' },
+      { text: 'Spigot', link: 'https://www.spigotmc.org/resources/signcolors.6135/' },
+      { text: 'GitHub', link: 'https://github.com/Pixelhash/SignColors' }
+    ]
+  }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,13 @@
-# Hello VuePress
+---
+home: true
+heroImage: ./hero.png
+actionText: Get Started ->
+actionLink: /guide/
+features:
+- title: Written in Kotlin
+  details: Less bugs and faster development.
+- title: Various Amount of Permissions
+  details: Allows to control the permission for each color and format.
+- title: Database Support
+  details: MySQL and SQLite database support out of the box.
+---
